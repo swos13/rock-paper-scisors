99557a98e8695cd66d3fedb09c6b7de63dc64bb1
swos <szymon.wos1319@gmail.com>
2023-12-01 19:25:10 +0100

    Create function that gets player's choice

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ console.log("Heyylo worldddd!");
 
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random()*3);
-    return randomNumber == 0 ? "rock" : randomNumber == 1 ? "paper" : "scissor";
+    return randomNumber == 0 ? "rock" : randomNumber == 1 ? "paper" : "scissors";
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice(){
+    let playerChoice = prompt("What is your choice (rock, paper or scissors?").toLowerCase();
+    while(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
+        playerChoice = prompt("This is not correct! What is your choice (rock, paper or scissors?").toLowerCase();
+    }
+    return playerChoice;
+}
+
+console.log(getPlayerChoice());
