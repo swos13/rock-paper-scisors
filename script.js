7b427f2dc7b9ef6269296b156035571a86c63989
swos <szymon.wos1319@gmail.com>
2023-12-09 21:31:37 +0100

    Change logic of the game
    Game logic should work with UI, so buttons need listeners and proper message should appear after each round and after game ends.

--- script.js
+++ script.js
@@ -11,21 +11,54 @@ function getPlayerChoice(){
     return playerChoice;
 }
 
+const buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0;
+let resultMessage = document.createElement('p');
+let score = document.querySelector('#score');
+let body = document.querySelector('body');
+body.appendChild(resultMessage);
+
 function playRound(computerChoice, playerChoice){
 
-    if(computerChoice == playerChoice)
-        console.log("It's a tie! Let's play this round again. ");
+    if(computerChoice == playerChoice){
+        resultMessage.textContent = "It's a tie! Let's play this round again.";
+    }
     else if((computerChoice == "rock" && playerChoice == "scissors") || 
     (computerChoice == "paper" && playerChoice == "rock") || 
-    (computerChoice == "scissors" && playerChoice == "paper"))
-        console.log(`Sorry, but ${computerChoice} beats ${playerChoice}. You lost this round! `);
-    else
-        console.log(`${playerChoice} beats ${computerChoice}. You won this round!`);
+    (computerChoice == "scissors" && playerChoice == "paper")){
+        computerScore++;
+        resultMessage.textContent = `Sorry, but ${computerChoice} beats ${playerChoice}. You lost this round! `;
+    }
+    else{
+        playerScore++;
+        resultMessage.textContent = `${playerChoice} beats ${computerChoice}. You won this round!`;
+    }
+    score.textContent = `Score: ${playerScore}:${computerScore}`;
 }
 
-const buttons = document.querySelectorAll('button');
+function endGame(){
+    if(playerScore == 3)
+        resultMessage.textContent = "Congratz! You win!";
+    else 
+        resultMessage.textContent = "You lost! Too bad.."; 
+    buttons.forEach((button) => {
+        button.remove();
+    })
+    let restartBtn = document.createElement('button');
+    restartBtn.textContent = "Play again";
+    body.appendChild(restartBtn);
+
+    restartBtn.addEventListener('click', () => {
+        window.location.reload();
+    });
+}
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        playRound(getComputerChoice(), button.value)})
+        playRound(getComputerChoice(), button.value);
+        if(playerScore == 3 || computerScore == 3)
+            endGame();
+    });    
 })
+
